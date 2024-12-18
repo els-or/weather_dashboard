34e1239b54efb9f050588ccf4c2252cc0326a0be
Rachel Sutton <raesak@gmail.com>
2024-12-18 10:47:02 -0800

    todos for weatherRoutes

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,19 +1,33 @@
-import { Router } from 'express';
+import { Router, type Request, type Response } from 'express';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
-// import WeatherService from '../../service/weatherService.js';
+import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
 
-// TODO: POST Request with city name to retrieve weather data
-router.post('/', (req, res) => {
-  // TODO: GET weather data from city name
-  // TODO: save city to search history
+// Done: POST Request with city name to retrieve weather data
+router.post('/', async (req: Request, res: Response) => {
+  const { cityName } = req.body;
+
+  // Done: GET weather data from city name
+  const weatherData = await WeatherService.getWeatherForCity(cityName)
+ // Done: save city to search history
+  await HistoryService.addCity(cityName);
+
+  res.json(weatherData);
+
 });
 
-// TODO: GET search history
-router.get('/history', async (req, res) => {});
+// Done: GET search history
+router.get('/history', async (_req: Request, res: Response) => {
+  const getCities = await HistoryService.getCities();
+  res.json(getCities);
+});
 
-// * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req, res) => {});
+// * BONUS Done: DELETE city from search history
+router.delete('/history/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
+  await HistoryService.removeCity(id);
+  res.sendStatus(204);
+});
 
 export default router;
